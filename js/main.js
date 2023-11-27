0191e30679ff82b5fa9c78680809c8df37050bc7
rafaelaafs <rafaelaamaral010@hotmail.com>
2023-11-26 21:26:25 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
 function buscarCEP() {
-    const cep = document.querySelector("#cep").value;
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((dados) => {
-        document.querySelector("#resultados").innerHTML = dados.logradouro + ", " + dados.bairro + ", " + dados.localidade + ", " + dados.uf;
-      });
-      console.log(response.json())
+  var cep = document.getElementById("cep").value;
+
+  cep = cep.replace(/\D/g, '');
+
+  if(cep.length !== 8 ){
+    alert('Por favor digite o CEP novamente');
+    return;
   }
-  
-  document.querySelector("#submit").addEventListener("click", buscarCEP);
-  
+
+  var url = 'https://viacep.com.br/ws/' + cep + '/json/';
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      Object.keys(data).forEach(key => {
+        if (!data[key]) {
+          data[key] = "Não informado"
+        }
+      });
+
+      document.getElementById('logradouro').value = data.logradouro;
+      document.getElementById('complemento').value = data.complemento;
+      document.getElementById('bairro').value = data.bairro;
+      document.getElementById('localidade').value = data.localidade;
+      document.getElementById('uf').value = data.uf;
+      document.getElementById('ddd').value = data.ddd;    
+      
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Erro na requisição:', error);
+      alert('Erro ao buscar o CEP. Por favor, tente novamente');
+    });
+}
